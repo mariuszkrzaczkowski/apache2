4f0cd1891dca02f26ef39792a1d78b2237a66173
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-02 19:26:50 +0200

    rtgtrg

--- apache.sh
+++ apache.sh
@@ -9,10 +9,10 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php
 sudo cp -f build/travis-ci-apache /etc/apache2/sites-available/000-default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default
 
-sudo cp -r -f /home/travis/build/mariuszkrzaczkowski/apache2/* /var/www/html
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 777 /var/www/html
-ls -all /var/www/html
+#sudo cp -r -f /home/travis/build/mariuszkrzaczkowski/apache2/* /var/www/html
+#sudo chown -R www-data:www-data /var/www/html
+#sudo chmod -R 777 /var/www/html
+ls -all 
 
 
 sudo service apache2 restart
