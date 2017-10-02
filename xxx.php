7e5f5942c65de14e22dbf477121229183c7a4216
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-02 19:42:45 +0200

    grggggggggggg

--- xxx.php
+++ xxx.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Bootstrap 99999999 Template <?php echo isset($_REQUEST['tttt'])?$_REQUEST['tttt']: '-';?></title>
+    <title>Bootstrap XXXXXXxxxx Template <?php echo isset($_REQUEST['tttt'])?$_REQUEST['tttt']: '-';?></title>
   </head>
   <body>
     <h1>Hello, world!@</h1>
